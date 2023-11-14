13b133013d41e4013879e6c553789307f526f8e8
lxdavis9lxd <lxdavis9lxd@gmail.com>
2023-11-14 06:31:42 +0000

    commit

--- index.js
+++ index.js
@@ -3,6 +3,70 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+console.log('Hello World!')
+const mysql = require('mysql2');
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+// Create a connection to the MySQL database
+const connection = mysql.createConnection({
+  host: '<hostname>',
+  user: '<username>',
+  password: '<password>',
+  database: '<database_name>'
+});
+
+// Connect to the MySQL database
+connection.connect();
+
+// Get the table metadata
+connection.query('SHOW TABLES', (error, results, fields) => {
+  if (error) throw error;
+
+  // Generate the Swagger file
+  const swagger = {
+    openapi: '3.0.0',
+    info: {
+      title: '<title>',
+      version: '<version>'
+    },
+    paths: {}
+  };
+
+  results.forEach((table) => {
+    const tableName = table[Object.keys(table)[0]];
+    swagger.paths[`/${tableName}`] = {
+      get: {
+        summary: `Get all ${tableName}`,
+        responses: {
+          200: {
+            description: `Returns all ${tableName}`,
+            content: {
+              'application/json': {
+                schema: {
+                  type: 'array',
+                  items: {
+                    $ref: `#/components/schemas/${tableName}`
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    };
+  });
+
+  // Write the Swagger file to disk
+  fs.writeFileSync('<filename>.yaml', yaml.dump(swagger));
+});
+
+// Close the connection to the MySQL database
+connection.end();
+
+
+
+
   res.send('Hello World!')
 })
 
